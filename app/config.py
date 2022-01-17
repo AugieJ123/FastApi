8b96e6710bb9deafd73fe8bac1053958f555dc4a
augiej123 <jibbanani@gmail.com>
2022-01-17 06:36:53 +0000

    ignoring some files

--- app/config.py
+++ app/config.py
@@ -3,13 +3,14 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     database_hostname: str
     database_port: str
-    database_password: str 
+    database_password: str
+    database_name: str
     database_username: str
     secret_key: str
     algorithm: str
-    access_token_minutes: int
+    access_token_minutes: int = 0
 
     class Config:
-        env_file = '.env'
+        env_file = "./.env"
 
 settings = Settings()
