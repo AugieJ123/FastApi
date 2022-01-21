24e4d8baac2a38435f314093530a869092e0cfb3
augiej123 <jibbanani@gmail.com>
2022-01-21 04:53:14 +0000

    delete phone number for users

--- app/models.py
+++ app/models.py
@@ -26,8 +26,6 @@ class User(Base):
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
-
-    phone_number = Column(String, nullable=False)
     
 class Vote(Base):
     __tablename__ = 'likes'
